4787cc7b063d2a242ddb05655f1afbaca335e81c
Zoltan Dezso <dezso.zoltan@gmail.com>
2011-11-20 02:42:49 +0900

    [BOARD] Adds dummy board (boards/me)

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ TND::Application.routes.draw do
 
   resources :items, :except=>:index
 
+  get 'boards/friends', :as=>:friends_board
+  get 'boards/me',      :as=>:my_board
+  get 'boards/nearby',  :as=>:nearby_board
+
   match '/auth/:provider/callback', :to => 'sessions#create'
   root :to=>'home#index'
 end
