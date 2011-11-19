bd776a312e3f938484540790bdf7cd757a88b523
Jordi Polo <jordi@cookpad.com>
2011-11-19 20:55:27 +0900

    real name for the bucket

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ActiveRecord::Base
   has_attached_file :photo,
                     :styles => { :medium => "300x300>", :thumb => "180x180>" },
                     :storage => :s3,
-                    :bucket => 'mybucket',
+                    :bucket => 'treasure.nd',
                     :s3_credentials => {
                       :access_key_id => ENV['S3_KEY_ID'],
                       :secret_access_key => ENV['S3_SECRET']
