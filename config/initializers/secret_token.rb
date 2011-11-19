99fd4b3e104e4e61c477725666fb347ca89e3835
Zoltan Dezso <dezso.zoltan@gmail.com>
2011-11-19 15:09:45 +0900

    Initial Commit - Application Skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TND::Application.config.secret_token = 'bfe9020d3f3fa74411a08c58633c2eb56b2d0a6738568f28112bf44c3d8253e06bf61084b17fd40ed2cc1d39df84afb4b62d87c051b07c12e37621f93c366c28'
