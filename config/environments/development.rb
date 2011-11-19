a0b832b5ba48432b40f2135930d4b4c5e1142baf
Jordi Polo <jordi@cookpad.com>
2011-11-19 18:16:09 +0900

    we have 2 facebook apps now, wooah, double action

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,9 @@ TND::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  #https://developers.facebook.com/apps/223244214412315
+  config.middleware.use OmniAuth::Builder do
+    provider :facebook, '223244214412315', '6cb16642bc5b464cc6b96d872d0df7a9'
+  end
 end
