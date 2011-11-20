bfe2bca2fc46287ced3fe78852438f4a9d54e244
Zoltan Dezso <dezso.zoltan@gmail.com>
2011-11-20 13:00:27 +0900

    [BOARD] Minor design changes and top menu rearrangement

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -4,6 +4,7 @@ class BoardsController < ApplicationController
   def friends
     # TODO: horrible hacks are on the rise with fewer and fewer hours remaining
     @items = User.find_by_name(['高田 美重子', '森田 一郎']).items
+    @current_board = '友達ボード'
   end
 
   def me
@@ -33,6 +34,8 @@ class BoardsController < ApplicationController
   end
 
   def nearby
+    @current_board = '近所ボード'
+    @items = User.find_by_name(['高田 美重子', '森田 一郎']).items
   end
 
 private
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 module ApplicationHelper
   ALERT_CLASSES = { :notice=>:success, :alert=>:error }
   def notices
@@ -7,4 +8,40 @@ module ApplicationHelper
     end
     raw output
   end
+
+  def tokyo_ward_select(selected='世田谷区')
+    wards = [
+             '足立区',
+             '荒川区',
+             '文京区',
+             '千代田区',
+             '中央区',
+             '江戸川区',
+             '板橋区',
+             '葛飾区',
+             '帰宅',
+             '江東区',
+             '目黒区',
+             '港区',
+             '中野区',
+             '練馬区',
+             '大田区',
+             '世田谷区',
+             '渋谷区',
+             '品川区',
+             '杉並区',
+             '墨田区',
+             '豊島区',
+             '台東区',
+             '新宿区',
+            ]
+    select_tag 'location', options_for_select(wards, selected)
+  end
+
+  def community_select(selected='')
+    communities = ['', 'VOYAGE GROUP', '東京大学']
+
+    select_tag 'community', options_for_select(communities, selected)
+  end
+
 end
